c4385969381ab98cbb0cbfa2a25307e26a23322e
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-10-22 23:47:05 +0800

    更新

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ExpermentWithControls
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
